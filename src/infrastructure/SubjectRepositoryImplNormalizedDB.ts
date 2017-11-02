cb0f5701dc3aec4c45d1a1087aa022ffab3f249f
yajamon <yajamon.tatsuki@gmail.com>
2017-11-03 01:12:37 +0900

    Add subject repository for normalized db skelton

---
+++ src/infrastructure/SubjectRepositoryImplNormalizedDB.ts
@@ -0,0 +1,17 @@
+import { Identity } from "../domain/identity.js";
+import { Subject } from "../domain/subject.js";
+import { SubjectRepository } from "../domain/subjectRepository.js";
+
+export class SubjectRepositoryImplNormalizedDB implements SubjectRepository {
+    public add(subject: Subject) {
+        // code
+    }
+
+    public update(subject: Subject) {
+        // code
+    }
+
+    public remove(id: Identity) {
+        // code
+    }
+}
