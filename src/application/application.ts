8519084305b14cbdd3ecde99447b87f2f3c67994
yajamon <yajamon.tatsuki@gmail.com>
2017-11-03 00:28:21 +0900

    Add factory method: Subject

--- src/application/application.ts
+++ src/application/application.ts
@@ -1,11 +1,8 @@
-import { Name } from "../domain/name.js";
 import { Subject } from "../domain/subject.js";
 
 export class Application {
     public createSubject(name: string): Subject {
-        const s = new Subject();
-        s.name = new Name(name);
-        return s;
+        return Subject.create("a subject");
     }
 
 }
--- src/domain/subject.ts
+++ src/domain/subject.ts
@@ -3,6 +3,12 @@ import { Name } from "./name.js";
 import { Tag } from "./tag.js";
 
 export class Subject extends Entity {
+    public static create(name: string) {
+        const s = new Subject();
+        s.name = new Name(name);
+        return s;
+    }
+
     public name: Name;
     public tags: Tag[];
 }
