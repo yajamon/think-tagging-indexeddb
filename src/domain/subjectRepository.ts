72857908944203acbc00d0ffb5737ab4c0142159
yajamon <yajamon.tatsuki@gmail.com>
2017-11-03 00:17:25 +0900

    Add interface of SubjectRepository instead to class of SubjectRepository

--- src/domain/subjectRepository.ts
+++ src/domain/subjectRepository.ts
@@ -1,15 +1,5 @@
-import { Identity } from "./identity.js";
 import { Repository } from "./repository.js";
 import { Subject } from "./subject.js";
 
-export class SubjectRepository implements Repository<Subject> {
-    public async add(subject: Subject) {
-        //
-    }
-    public async remove(id: Identity) {
-        //
-    }
-    public async update(subject: Subject) {
-        //
-    }
+export interface SubjectRepository extends Repository<Subject> {
 }
