3901e894596f7feb1e9adccfecc741c472107d27
yajamon <yajamon.tatsuki@gmail.com>
2019-11-07 18:58:19 +0900

    SubjectRepository関連のエラーを修正

--- src/application/application.ts
+++ src/application/application.ts
@@ -1,8 +1,10 @@
 import { Subject } from "../domain/subject.js";
+import { SubjectRepositoryImplNormalizedDB } from "../infrastructure/SubjectRepositoryImplNormalizedDB.js";
 
 export class Application {
+    subjectRepo = new SubjectRepositoryImplNormalizedDB()
     public createSubject(name: string): Subject {
-        return Subject.create("a subject");
+        return Subject.create(this.subjectRepo, "a subject");
     }
 
 }
--- src/infrastructure/SubjectRepositoryImplNormalizedDB.ts
+++ src/infrastructure/SubjectRepositoryImplNormalizedDB.ts
@@ -3,6 +3,12 @@ import { Subject } from "../domain/subject.js";
 import { SubjectRepository } from "../domain/subjectRepository.js";
 
 export class SubjectRepositoryImplNormalizedDB implements SubjectRepository {
+    private innerId: number = 0;
+    nextId(): Identity {
+        this.innerId += 1;
+        return new Identity(this.innerId);
+    }
+
     public add(subject: Subject) {
         // code
     }
